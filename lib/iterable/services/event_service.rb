50de30d63cb88972e38334928099f5b79708e1e3
Matt Brand <agmattbrand@yahoo.com>
2017-08-18 12:22:23 -0400

    Add debug

--- lib/iterable/services/event_service.rb
+++ lib/iterable/services/event_service.rb
@@ -9,6 +9,9 @@ module Iterable
     class EventService < BaseService
       class << self
         def track(request)
+          puts "********** DEBUG START"
+          puts request
+          puts "********** DEBUG END"
           raise Exceptions::ServiceException, "Must be a Iterable::Requests::TrackEvent" unless request.is_a?(Iterable::Requests::TrackEvent)
           post(Util::Config.get('endpoints.track_event'), request)
         end

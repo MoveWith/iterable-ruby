79a16aa2a0678fd11a0934269f2090896a37b087
Matt Brand <agmattbrand@yahoo.com>
2017-08-18 16:37:28 -0400

    Send as an integer

--- lib/iterable/models/target_email.rb
+++ lib/iterable/models/target_email.rb
@@ -1,6 +1,6 @@
 module Iterable
   class TargetEmail < Iterable::Base
-    property :campaignId, required: true, coerce: String
+    property :campaignId, required: true, coerce: Integer
     property :recipientEmail, required: true, coerce: String
     property :dataFields, coerce: DataFields
   end
